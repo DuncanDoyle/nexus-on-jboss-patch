beccb2b749dcdd59ada81466c82a6bd118f8d095
Duncan Doyle <Duncan.Doyle@gmail.com>
2014-04-03 15:42:45 +0200

    Added jboss-web.xml to set the context-root to '/nexus'.

--- patch-nexus.sh
+++ patch-nexus.sh
@@ -3,9 +3,11 @@
 #
 # Adds the jboss-deployment-structure.xml to the WEB-INF folder of the Nexus WAR (provided as the argument to this script).
 # This fixes a deployment issue with Nexus on JBoss AS7/EAP6.
+# 
+# Also adds a jboss-web.xml which sets the context-root to "nexus" (so you don't have to specify the nexus version in your URL).
 #
 # Requires the 'zip' tool to be installed on the system.
 #
 # author: duncan.doyle@redhat.com
 #
-zip $1 WEB-INF/jboss-deployment-structure.xml
+zip $1 WEB-INF/*
